750d0a72e0bb3b307fbf11785fa2a04fa7e8eab6
QURRATUL AYEN ELMA <115572377+chyelma@users.noreply.github.com>
2023-11-21 18:37:51 +0600

    Add files via upload

---
+++ my_project copy/customuser/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import CustomUser
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from .models import CustomUser
+
+# Create your views here.
